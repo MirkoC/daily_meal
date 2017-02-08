a3368c08d99c38250ee1328fa7b6888a2240877a
Mirko Cerovac <mirko.cerovac@gmail.com>
2017-02-08 09:12:51 +0100

    Updated seeds with real restaurant location data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,21 @@ FactoryGirl.create(:user, email: 'test@example.example', password: '12345678')
     FactoryGirl.create(:meal, restaurant: restaurant)
   end
 end
+
+10.times do
+  restaurant = FactoryGirl.create(:restaurant, city: 'Zagreb',
+                                               latitude: Faker::Number.normal(45.815011, 0.05),
+                                               longitude: Faker::Number.normal(15.981919, 0.05))
+  Faker::Number.between(15, 25).times do
+    FactoryGirl.create(:meal, restaurant: restaurant)
+  end
+end
+
+10.times do
+  restaurant = FactoryGirl.create(:restaurant, city: 'Ljubljana',
+                                               latitude: Faker::Number.normal(46.056947, 0.05),
+                                               longitude: Faker::Number.normal(14.505751, 0.05))
+  Faker::Number.between(15, 25).times do
+    FactoryGirl.create(:meal, restaurant: restaurant)
+  end
+end
