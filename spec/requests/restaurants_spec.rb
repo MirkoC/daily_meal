4b2e160ec416559854b609035e53d10466e8e983
Mirko Cerovac <mirko.cerovac@gmail.com>
2017-02-09 14:57:34 +0100

    Test seed data extracted to modular context

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -1,49 +1,14 @@
 require_relative 'api_helper'
+require_relative '../test_seed'
 
 RSpec.describe RestaurantsController do
   include_context 'api_helper'
+  include_context 'seed_data'
 
   let(:user) { create(:user) }
   let(:headers) { { 'Content-Type' => 'application/json',
                     'Accept' => 'application/json',
                     'Authorization' => "Bearer #{user.new_jwt}" } }
-  let!(:restaurant_1) { create(:restaurant, name: 'Simple Restaurant', city: 'Zagreb', latitude: 45.71101, longitude: 15.92111) }
-  let!(:restaurant_2) { create(:restaurant, name: 'Test Diner', city: 'Zagreb', latitude: 45.72, longitude: 16.5 ) }
-  let!(:restaurant_3) { create(:restaurant, name: 'Simple Pancakes', city: 'Ljubljana', latitude: 46.055, longitude: 14.505) }
-  let!(:restaurant_4) { create(:restaurant, name: 'Test Restaurant', city: 'Ljubljana', latitude: 46.07, longitude: 14.509) }
-  let!(:restaurant_5) { create(:restaurant, name: 'Simple Diner', city: 'Ljubljana', latitude: 46.03, longitude: 14.511) }
-  let!(:meal_names_1) { ['Beans in Stew with Paper', 'Carrot in Water with Salt', 'Meat in Potato with Cinemon Powder', 'Beans in Potato with Salt'] }
-  let!(:meal_names_2) { ['Onion in Duck with Ginger Root', 'Lettuce in Water with Curry', 'Meat in Potato with Cinemon Powder', 'Tomato in Potato with Salt'] }
-
-  let!(:meals_1) do
-    meal_names_1.each do |name|
-      create(:meal, restaurant: restaurant_1, name: name)
-    end
-  end
-
-  let!(:meals_2) do
-    meal_names_2.each do |name|
-      create(:meal, restaurant: restaurant_2, name: name)
-    end
-  end
-
-  let!(:meals_3) do
-    meal_names_1.each do |name|
-      create(:meal, restaurant: restaurant_3, name: name)
-    end
-  end
-
-  let!(:meals_4) do
-    meal_names_2.each do |name|
-      create(:meal, restaurant: restaurant_4, name: name)
-    end
-  end
-
-  let!(:meals_5) do
-    meal_names_1.each do |name|
-      create(:meal, restaurant: restaurant_5, name: name)
-    end
-  end
 
   describe :index do
     it 'gets all of the restaurants' do
